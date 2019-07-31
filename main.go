dd8abf471028ec73efe8cb748077ca027773ca72
Cristian Radu <cristian.andrei.radu@gmail.com>
2019-07-31 16:54:29 +0100

    initialize and start domain controller

--- main.go
+++ main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/cristian-radu/banyan/pkg/apis/banyan/v1alpha1"
+	"github.com/cristian-radu/banyan/pkg/controller/domain"
+	clientset "github.com/cristian-radu/banyan/pkg/generated/clientset/versioned"
+	informers "github.com/cristian-radu/banyan/pkg/generated/informers/externalversions"
 
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apiextensionsclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/klog"
 )
 
 func main() {
 
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		panic(err)
+		klog.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	banyanClient, err := clientset.NewForConfig(config)
+	if err != nil {
+		klog.Fatalf("Error building domain clientset: %s", err.Error())
 	}
 
 	apiextensionsClient, err := apiextensionsclientset.NewForConfig(config)
@@ -28,13 +43,33 @@ func main() {
 		panic(err)
 	}
 
+	stopCh := make(chan struct{})
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGKILL)
 
-	sig := <-sigs
-	log.Printf("Received signal %v, exiting.", sig)
-	os.Exit(0)
+	domainInformerFactory := informers.NewSharedInformerFactory(banyanClient, time.Second*30)
 
+	domainController, err := domain.NewController(
+		kubeClient,
+		banyanClient,
+		domainInformerFactory.Banyan().V1alpha1().Domains(),
+	)
+
+	if err != nil {
+		klog.Fatalf("Error creating the domain controller: %s", err.Error())
+	}
+
+	domainInformerFactory.Start(stopCh)
+	domainInformerFactory.WaitForCacheSync(stopCh)
+	
+	// ToDo: configurable number of workers
+	if err = domainController.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running domain controller: %s", err.Error())
+	}
+
+	sig := <-sigs
+	klog.Infof("Received signal %v, exiting.", sig)
+	close(stopCh)
 }
 
 func createCRD(client *apiextensionsclientset.Clientset, CRDIn *apiextensionsv1beta1.CustomResourceDefinition) (*apiextensionsv1beta1.CustomResourceDefinition, error) {
