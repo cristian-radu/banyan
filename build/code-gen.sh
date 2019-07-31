664d271141d08ee4a7e4d8b7ba3177fd9be06c7a
Cristian Radu <cristian.andrei.radu@gmail.com>
2019-07-31 16:49:31 +0100

    fix codegen wrong import paths generated

--- build/code-gen.sh
+++ build/code-gen.sh
@@ -9,21 +9,21 @@ declare -r projectOwner="github.com/cristian-radu"
 declare -r binaryName="banyan"
 declare -r apiVersion="v1alpha1"
 declare -r apisPath="${projectOwner}/${binaryName}/pkg/apis"
-declare -r codeGenDirName=$(grep k8s.io/code-generator go.mod | sed -E -e 's/^[[:blank:]]|[[:blank:]]$$//' -e s'/[[:blank:]]/@/')
-declare -r codeGenDirPath="${GOPATH}/pkg/mod/${codeGenDirName}"
-
 
 function downloadCodeGenerator() {
     go mod download k8s.io/code-generator
 }
 
 function runCodeGenerator() {
+    declare -r codeGenDirName="k8s.io/code-generator@$(grep "replace k8s.io/code-generator" go.mod | awk '{print $5}')"
+    declare -r codeGenDirPath="${GOPATH}/pkg/mod/${codeGenDirName}"
+
     # Remove the old generated code
     rm -rf ${projectRoot}/pkg/generated
 
     # Run the generators
     chmod +x ${codeGenDirPath}/generate-groups.sh
-	${codeGenDirPath}/generate-groups.sh all ./${projectOwner}/${binaryName}/pkg/generated ${apisPath} ${binaryName}:${apiVersion} -o ${projectRoot}
+	${codeGenDirPath}/generate-groups.sh all ${projectOwner}/${binaryName}/pkg/generated ${apisPath} ${binaryName}:${apiVersion} -o ${projectRoot}
 }
 
 function cleanup() {
