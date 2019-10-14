1b838b7f90225f4a3fc515db355b000c9c5b9625
Cristian Radu <cristian.andrei.radu@gmail.com>
2019-10-14 16:06:33 +0100

    updates to registrar interface

--- pkg/registrar/registrar.go
+++ pkg/registrar/registrar.go
@@ -1,5 +1,11 @@
 package registrar
 
+import (
+	"github.com/cristian-radu/banyan/pkg/apis/banyan/v1alpha1"
+)
+
+// Interface is the interface that must be implemented by a registrar in order to be used with the domain controller
 type Interface interface {
-	isDomainAvailable(name string) (bool, error)
+	IsNameAvailable(d v1alpha1.Domain) (bool, error)
+	RegisterDomain(d v1alpha1.Domain) error
 }
